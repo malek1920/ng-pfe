b4c93317ce028c8ec57a8ce7a169278f193bef4b
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2021-05-28 00:12:03 +0100

    28-05-2021

--- src/app/components/dashboard/addbilans/addbilans.component.ts
+++ src/app/components/dashboard/addbilans/addbilans.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { SnotifyService } from 'ng-snotify';
 import { AuthService } from '../../../services/authservices/auth.service';
 import { OffreService } from '../../../services/offre/offre.service';
+import { MvcomptableService } from 'src/app/services/mvcomptable/mvcomptable.service';
 
 @Component({
   selector: 'app-addbilans',
@@ -10,24 +11,37 @@ import { OffreService } from '../../../services/offre/offre.service';
   styleUrls: ['./addbilans.component.css']
 })
 export class AddbilansComponent implements OnInit {
+  listeMvComptable;
   public error = [] as any;
   public form = {
     nom: null,
     code: null,
     etat: null,
     mv_comptable_id: null,
+    actif: null,
+    passif: null,
+    total_actif: null,
+    total_passif: null,
   }
+
   constructor(
     private Notfiy: SnotifyService,
     private authService:AuthService,
     private offreService: OffreService,
+    private mvcomptableService: MvcomptableService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
     // set user id
     const user_info = this.authService.getLoggedUserInfo();
-    
+    this.getData();
+  }
+
+  getData() {
+    this.mvcomptableService.getListeMvComptable().subscribe((res: any[]) => {
+      this.listeMvComptable = res['Movement Comptable'];
+    })
   }
 
   addbilans() {
